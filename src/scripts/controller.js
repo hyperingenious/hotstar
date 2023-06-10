3b51228fbf1f2c060e6c6f4a8f3b6b876514c1e7
Saurav Meghwal <skbmasale941@gmail.com>
2023-06-10 17:26:45 +0530

    The top section look dope

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -3,6 +3,5 @@ import asideView from "../scripts/view/asideView.js";
 const init = function () {
     asideView.mouseoverHandler()
     asideView.mouseoutHandler()
-    asideView.svgGlowHandler()
 };
 init()
